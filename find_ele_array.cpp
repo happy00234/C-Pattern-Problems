3f4445c727d407beff26afff9a6595dfab86cb8e
Happy Sharma <sharmahappy93508@gmail.com>
2024-05-05 13:29:15 +0530

    Finding an element in array

---
+++ find_ele_array.cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>
+using namespace std;
+
+bool search(int arr[],int size,int key){
+    for(int i = 0;i<size;i++){
+        if(arr[i] == key){
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int main() {
+    int arr[8] = {5,7,-2,10,20,-12,-15,100};
+    int key;
+    cin >> key;
+    
+    bool found =  search(arr,8,key);
+    if(found){
+        cout<< "key is present";
+        
+    }
+    else{
+        cout<< "key is not present";
+    }
+    
+
+   
+}
